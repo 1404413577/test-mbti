8a0307a3b9432aedd3f1125ab3004e14f113ee57
lion <1404413577@qq.com>
2026-05-06 13:54:20 +0800

    hg

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  // 核心魔法：如果是在 GitHub Actions 中打包，就用项目名；否则（本地/Vercel）用根路径 '/'
+  base: process.env.GITHUB_ACTIONS ? '/test-mbti/' : '/',
+  
   plugins: [vue()],
   resolve: {
     alias: {
@@ -16,15 +17,9 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('element-plus')) {
-              return 'element-plus'
-            }
-            if (id.includes('@element-plus/icons-vue')) {
-              return 'element-icons'
-            }
-            if (id.includes('vue') || id.includes('vue-router') || id.includes('vue-i18n')) {
-              return 'vue-vendor'
-            }
+            if (id.includes('element-plus')) return 'element-plus'
+            if (id.includes('@element-plus/icons-vue')) return 'element-icons'
+            if (id.includes('vue') || id.includes('vue-router') || id.includes('vue-i18n')) return 'vue-vendor'
           }
         }
       }
